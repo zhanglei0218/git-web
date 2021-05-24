66a4e4025e95b2319e5ad54d795a53df77dadcb0
zhanglei <2034319129@qq.com>
2021-05-24 23:09:29 +0800

    first commit

---
+++ src/main/java/MyGit.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zhanglei
+ * Date 2021/5/24 23:08
+ * Description:
+ */
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸");
+    }
+}
