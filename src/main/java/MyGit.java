083490c3a78d82291b8a6ff14b6ccb8f47e8940c
zhanglei <2034319129@qq.com>
2021-05-25 14:30:42 +0800

    我是user分支第二次提交

--- src/main/java/MyGit.java
+++ src/main/java/MyGit.java
@@ -6,7 +6,6 @@
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("我是你爸爸1");
-
-        System.out.println("我是user分支");
+        System.out.println("算法要考试了");
     }
 }
